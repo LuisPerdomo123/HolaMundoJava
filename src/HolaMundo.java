51f40a71962fd5d8f735419e705e2fa1b6087f73
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-20 10:15:12 -0500

    Ejemplo 2 sentencia if else - Calculo estacion anio

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -445,5 +445,24 @@ public class HolaMundo {
         }
 
         System.out.println("numeroTexto = " + numeroTexto);
+
+        //Segundo ejemplo if else para estacion del anio
+        var mes = 1;
+        var estacion = "Estacion desconocida";
+
+        if (mes == 1 || mes == 2 || mes == 12) {
+
+            estacion = "Invierno";
+        } else if (mes == 3 || mes == 4 || mes == 5) {
+
+            estacion = "Primavera";
+        } else if (mes == 6 || mes == 7 || mes == 8) {
+
+            estacion = "Verano";
+        } else if (mes == 9 || mes == 10 || mes == 11) {
+            estacion = "Otonio";
+        }
+        System.out.println("estacion = " + estacion);
+
     }
 }
