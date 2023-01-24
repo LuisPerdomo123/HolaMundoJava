77f6241a59a426818821fe6d03b3b2b0544b4a55
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-24 09:57:31 -0500

    Atributos y metodos

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -9,5 +9,18 @@ package clases;
  * @author lepo9
  */
 public class Persona {
+    /*
+    Definicion de atributos de la clase
+    */
+    String nombre;
+    String apellido;
     
+    /*
+    Definicion de los metodos de la clase
+    */
+    public void desplegarInformacion(){//Void significa que no retorna informacion
+    
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: " + apellido);
+    }
 }
