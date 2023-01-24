f83a16d87266a83216bf5248fdfb73008957a6e2
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-24 10:32:56 -0500

    Objetos parte II

--- src/clases/PruebaPersona.java
+++ src/clases/PruebaPersona.java
@@ -12,12 +12,24 @@ public class PruebaPersona {
     
     public static void main(String[] args) {
      
-        Persona persona1; //Creacion de la variable persona del tipo Persona
+        Persona persona1; //Creacion de la variable persona del tipo Persona. 
         persona1= new Persona();//Llamada del constructor de la clase. Permite asignar variables y reservar memoria.
         //Se ha creado un objeto tipo Persona. Retorna la referencia asignada a la variable persona1.
         
         persona1.nombre = "Juan";
         persona1.apellido = "Perez";
         persona1.desplegarInformacion();
+        
+        //Creacion de un nuevo objeto
+        Persona persona2 = new Persona();
+        //Posicion de memoria de los objetos
+        System.out.println("persona1 = " + persona1);
+        System.out.println("persona2 = " + persona2);
+        //Inicialmente se imprime el valor de null. Ausencia de valor.
+        persona2.desplegarInformacion();
+        persona2.nombre = "Karla";
+        persona2.apellido = "Lara";
+        persona2.desplegarInformacion();
+        
     }
 }
