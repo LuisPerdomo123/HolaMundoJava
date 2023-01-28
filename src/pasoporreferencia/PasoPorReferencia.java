c0a35ea9bf2f3e7aa15f8ddb2a08b82f80290864
LuisPerdomo123 <leperdomoo@correo.udistrital.edu.co>
2023-01-27 20:22:43 -0500

    Uso de la palabra return y null

--- src/pasoporreferencia/PasoPorReferencia.java
+++ src/pasoporreferencia/PasoPorReferencia.java
@@ -18,16 +18,25 @@ public class PasoPorReferencia {
         persona1.nombre = "Juan";
         System.out.println("personal nombre: " + persona1.nombre);//Esta no es la mejor practica para modificar y retornar valores
         
-        cambiarValor(persona1);
+        persona1 = cambiarValor(persona1);
         System.out.println("persona1 cambio nombre:  = " + persona1.nombre);
     }
     
-    public static void cambiarValor(Persona persona){
+    
+    //Se puede regresar referencias de objetos
+    //La palabra return siempre este en el metodo. En uno metodo void, solo se tiene return;
+    public static Persona cambiarValor(Persona persona){
     
         //Se esta modiificando el valor de memoria heap en el objeto. El objeto tiene la misma referencia de espacio de memoria.
         //Se esta apuntando al espacio de memoria del objeto.
         //Paso por valor por referencia
-        persona.nombre = "Karla";
+        if(persona == null){
         
+            System.out.println("Valor de persona invalido ;  null");
+            return null;
+        }
+        
+        persona.nombre = "Karla";
+        return persona;
     }
 }
